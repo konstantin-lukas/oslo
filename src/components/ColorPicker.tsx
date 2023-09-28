066145c1f13a61f14b793a3f1b0177292f8ca46d
Konstantin Lukas <konstantin_lukas@web.de>
2023-09-28 15:34:02 +0200

    add light mode to color picker

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,7 +1,8 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useContext, useEffect, useMemo, useRef, useState} from "react";
 import Pickr from "@simonwep/pickr";
 import './ColorPicker.scss';
 import {useTheme} from "styled-components";
+import {LightModeContext} from "./misc/Contexts";
 
 export default function ColorPicker({onInput, accountId} : {onInput: (color: string) => void, accountId: number}) {
     const theme = useTheme();
@@ -9,6 +10,7 @@ export default function ColorPicker({onInput, accountId} : {onInput: (color: str
     const pickerElement = useRef(null);
     const [picker, setPicker] = useState(null);
     const [selectedColor, setSelectedColor] = useState(defaultColor);
+    const lightMode = useContext(LightModeContext);
     let mounted = false;
     useEffect(() => {
         if (pickerElement.current && selectedColor && !mounted) {
@@ -71,13 +73,24 @@ export default function ColorPicker({onInput, accountId} : {onInput: (color: str
             onInput(selectedColor);
     }, [selectedColor]);
 
+    const lightModeStyle = useMemo(() => lightMode ? <style>
+        {`.pcr-app .pcr-interaction .pcr-result {background: #eee;color: #1a1a1a;}`}
+        {`.pcr-app .pcr-interaction .pcr-save, .pcr-app .pcr-interaction .pcr-cancel {background: #444;color: #fff;}`}
+        {`.pcr-app .pcr-interaction .pcr-save:hover, .pcr-app .pcr-interaction .pcr-cancel:hover {background: #1a1a1a;}`}
+        {`.custom-color-picker {background: #ffffff;box-shadow: rgba(0,0,0,.5) 0 0 10px}`}
+    </style> : <></>, [lightMode]);
+
+    console.log(lightModeStyle)
     return (
-        <span
-            id="color_picker"
-            style={{
-                background: selectedColor
-            }}
-            ref={pickerElement}
-        ></span>
+        <>
+            {lightModeStyle}
+            <span
+                id="color_picker"
+                style={{
+                    background: selectedColor
+                }}
+                ref={pickerElement}
+            ></span>
+        </>
     );
 }
