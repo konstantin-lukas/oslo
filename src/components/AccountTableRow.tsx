a2ee3a09581d7e6659ad0b3e93feb79295fae2c5
Konstantin Lukas <konstantin_lukas@web.de>
2024-08-24 13:38:27 +0200

    add option to update transaction sum after creation

--- src/components/AccountTableRow.tsx
+++ src/components/AccountTableRow.tsx
@@ -1,10 +1,10 @@
-import React, {useContext, useDeferredValue, useEffect, useMemo, useRef, useState} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 import {createPortal} from "react-dom";
 import {styled, ThemeContext} from "styled-components";
 import {AlertContext, CurrencyContext, LanguageContext, TextContext} from "./misc/Contexts";
 import './AccountTableRow.scss';
-import {Money, MoneyFormatter} from "moneydew";
 import {useDefer} from "anzol";
+import CurrencyInput from "./CurrencyInput";
 
 const StyledButton = styled.button`
   background: ${props => props.theme.theme_color};
@@ -32,42 +32,46 @@ export default function AccountTableRow({id, sum, title, timestamp, category, fe
     const text = useContext(TextContext);
     const theme = useContext(ThemeContext);
 
-    const className = "balanceChange" + (sum[0] === '-' ? " expense" : " proceed");
     const date = new Date(timestamp).toLocaleDateString(language.code);
-    const formattedSum = useMemo(() => {
-        return new MoneyFormatter({
-            displayOrder: language.format.displayOrder,
-            currencySymbol: currency.symbol,
-            symbolSeparator: language.format.symbolSeparator,
-            currencyName: '',
-            negativeSign: '-',
-            positiveSign: '+'
-        }).format(new Money(sum));
-    }, [sum, language, currency]);
     const [transactionTitle, setTransactionTitle] = useState(title);
     const [transactionCategory, setTransactionCategory] = useState(category);
+    const [transactionSum, setTransactionSum] = useState(sum);
     const [showModal, setShowModal] = useState(false);
     const [showModalTimeout, setShowModalTimeout] = useState<NodeJS.Timeout>();
     const isMountingRef = useRef(false);
     const deferredTitle = useDefer(transactionTitle, 800);
     const deferredCategory = useDefer(transactionCategory, 800);
+    const deferredSum = useDefer(transactionSum, 800);
     useEffect(() => {
         isMountingRef.current = true;
     }, []);
     useEffect(() => {
         if (!isMountingRef.current) {
-            api.db.patchTransaction(transactionTitle, sum, id, transactionCategory);
-            setShowModal(true);
-            if (showModalTimeout) clearTimeout(showModalTimeout);
-            setShowModalTimeout(setTimeout(() => setShowModal(false), 1000));
+            api.db.patchTransaction(transactionTitle, transactionSum, id, transactionCategory).then(() => {
+                setShowModal(true);
+                if (showModalTimeout) clearTimeout(showModalTimeout);
+                setShowModalTimeout(setTimeout(() => setShowModal(false), 1000));
+                fetchTransactions();
+            });
         } else {
             isMountingRef.current = false;
         }
-    }, [deferredTitle, deferredCategory]);
+    }, [deferredTitle, deferredCategory, deferredSum]);
     return (
-        <tr className={className}>
-            <td colSpan={4} title={formattedSum}>
-                <span>{formattedSum}</span>
+        <tr className={"balanceChange" + (transactionSum[0] === '-' ? " expense" : " proceed")}>
+            <td colSpan={4} title={transactionSum}>
+                <span><CurrencyInput
+                    setValue={setTransactionSum}
+                    value={transactionSum}
+                    customFormat={{
+                        displayOrder: language.format.displayOrder,
+                        currencySymbol: currency.symbol,
+                        symbolSeparator: language.format.symbolSeparator,
+                        currencyName: '',
+                        negativeSign: '-',
+                        positiveSign: '+'
+                    }}
+                /></span>
             </td>
             <td colSpan={10} title={title ?? text.not_set_}>
                 <input type="text" placeholder={text.not_set_} value={transactionTitle ?? ""} onInput={(e) => {
